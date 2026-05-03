7eae83a8181403ed014f2bf8f70c032fa243ff8a
Philip Dubé <159546+serprex@users.noreply.github.com>
2026-05-03 22:50:55 +0000

    mongo: export container logs as artifacts
    also increase startsecs from 1s to 5s since CI environments yield for long durations
    
    this revealed issue where catching up should preserve UUIDs

--- tests_func/helpers/mongo.go
+++ tests_func/helpers/mongo.go
@@ -604,12 +604,10 @@ func (mc *MongoCtl) GetLogs() error {
 }
 
 func (mc *MongoCtl) PurgeDatadir() error {
-	err := mc.StopMongod()
-	if err != nil {
+	if err := mc.StopMongod(); err != nil {
 		return err
 	}
-	_, err = mc.runCmd("bash", "-c", "rm -rf /var/lib/mongodb/*")
-	if err != nil {
+	if _, err := mc.runCmd("bash", "-c", "rm -rf /var/lib/mongodb/*"); err != nil {
 		return err
 	}
 
@@ -622,23 +620,20 @@ func (mc *MongoCtl) ChownDBPath() error {
 }
 
 func (mc *MongoCtl) ChangeReplSet(rs string) error {
-	_, err := mc.runCmd("bash", "-c",
+	if _, err := mc.runCmd("bash", "-c",
 		fmt.Sprintf("sed -i 's/--replSet [^ ]*/--replSet %s/' /etc/supervisor/conf.d/mongodb.conf", rs),
-	)
-	if err != nil {
+	); err != nil {
 		return err
 	}
-	_, err = mc.runCmd("supervisorctl", "reread")
-	if err != nil {
+	if _, err := mc.runCmd("supervisorctl", "reread"); err != nil {
 		return err
 	}
-	_, err = mc.runCmd("supervisorctl", "update")
-	if err != nil {
+	if _, err := mc.runCmd("supervisorctl", "update"); err != nil {
 		return err
 	}
-	err = mc.StopMongod()
-	if err != nil {
+	if err := mc.StopMongod(); err != nil {
 		return err
 	}
 	return mc.StartMongod()
 }
+
